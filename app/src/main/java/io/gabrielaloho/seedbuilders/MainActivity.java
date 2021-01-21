9cfcd3e8ec766e54caf4feadf9886ec9c6a6f214
GABRIEL ALOHO <alohoaondoaver@gmail.com>
2021-01-21 11:46:25 +0100

    Initial commit

---
+++ app/src/main/java/io/gabrielaloho/seedbuilders/MainActivity.java
@@ -0,0 +1,26 @@
+package io.gabrielaloho.seedbuilders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(webView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://www.seedbuildersng.com");
+
+    }
+}
